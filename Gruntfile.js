fd35f4bd9f29f46e1f704768fb65a9006c9fbb46
Chris Laughlin <bluntm@gmail.com>
2015-03-14 15:06:38 +0000

    adding grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,54 @@
 module.exports = function(grunt) {
 
+    // Load Grunt tasks declared in the package.json file
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+        express: {
+            all: {
+                options: {
+                    port: 9000,
+                    hostname: "0.0.0.0",
+                    bases: ['.'], // Replace with the directory you want the files served from
+                                        // Make sure you don't use `.` or `..` in the path as Express
+                                        // is likely to return 403 Forbidden responses if you do
+                                        // http://stackoverflow.com/questions/14594121/express-res-sendfile-throwing-forbidden-error
+                    livereload: true
+                }
+            }
+        },
+
+        // grunt-watch will monitor the projects files
+        watch: {
+            all: {
+                // Replace with whatever file you want to trigger the update from
+                // Either as a String for a single entry
+                // or an Array of String for multiple entries
+                // You can use globing patterns like `css/**/*.css`
+                // See https://github.com/gruntjs/grunt-contrib-watch#files
+                files: 'index.html',
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        // grunt-open will open your browser at the project's URL
+        open: {
+            all: {
+                // Gets the port from the connect configuration
+                path: 'http://localhost:<%= express.all.options.port%>'
             }
         }
     });
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    // Creates the `server` task
+    grunt.registerTask('server', [
+        'express',
+        'open',
+        'watch'
+    ]);
 
 };
